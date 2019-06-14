a8054b2d38f12349e64c9c302f21c3b2593bafa5
Yan Zhang <cyanogenoid@cyanogenoid.com>
2019-06-14 07:42:57 +0100

    Use proper ue umlaut

--- fspool.py
+++ fspool.py
@@ -8,7 +8,7 @@ class FSPool(nn.Module):
         Featurewise sort pooling. From:
 
         FSPool: Learning Set Representations with Featurewise Sort Pooling.
-        Yan Zhang, Jonathon Hare, Adam Pr\"ugel-Bennett
+        Yan Zhang, Jonathon Hare, Adam Prügel-Bennett
         https://arxiv.org/abs/1906.02795
     """
     def __init__(self, in_channels, n_pieces, relaxed=False):
